eb3d092f62a5a6e196b3c56afe70d2dcefccd154
David Basil <info@basildavid.com>
2019-06-23 18:10:17 +0400

    add colors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-  <h2>Hi there</h2>
+
+<h1 class="text-capitalize text-center">text colors</h1>
+<p class="text-primary">hello bootstrap</p>
+<p class="text-success">hello bootstrap</p>
+<p class="text-info">hello bootstrap</p>
+<p class="text-warning">hello bootstrap</p>
+<p class="text-danger">hello bootstrap</p>
+<p class="text-secondary">hello bootstrap</p>
+<p class="text-dark">hello bootstrap</p>
+<p class="text-white bg-dark">hello bootstrap</p>
+<p class="text-light bg-dark">hello bootstrap</p>
+<p class="text-muted">hello bootstrap</p>
+
+<h1 class="text-capitalize text-center bg-gradient-success">background colors</h1>
+<p class="bg-primary">hello bootstrap</p>
+<p class="bg-success">hello bootstrap</p>
+<p class="bg-info">hello bootstrap</p>
+<p class="bg-warning">hello bootstrap</p>
+<p class="bg-danger">hello bootstrap</p>
+<p class="bg-secondary">hello bootstrap</p>
+<p class="bg-dark text-warning">hello bootstrap</p>
+<p class="bg-light">hello bootstrap</p>
+
 @endsection
